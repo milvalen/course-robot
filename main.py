d94ceb00b67c30e1391cd33c90f446a7e033fe31
milvalen <artemyerkov@gmail.com>
2024-09-23 18:21:49 +0300

    fix log sleep

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ import csv
 import os
 
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from multiprocessing import Process
 from selenium import webdriver
 
-INITIAL_URL = ''
-
-
-def clickAuth(driver: WebDriver): driver.find_elements(By.TAG_NAME, 'button')[1].click()
+INITIAL_URL = 'https://edubasmik.lms.2035.university/viewer/sessions/217/materials/3467'
 
 
 def get_profile_path(email):
@@ -29,8 +25,8 @@ def click_next_page(driver):
     print(button)
 
 
-def process_material(driver):
-    driver.switch_to.frame(driver.find_element(By.CLASS_NAME, 'materialFrame'))
+def process_material(driver, email):
+    driver.switch_to.frame(driver.find_element(By.TAG_NAME, 'iframe'))
 
     pdf_elements = driver.find_elements(By.CLASS_NAME, 'react-pdf__Document')
     video_elements = driver.find_elements(By.TAG_NAME, 'video')
@@ -38,21 +34,21 @@ def process_material(driver):
     if pdf_elements:
         page_count = len(pdf_elements[0].find_elements(By.TAG_NAME, 'div'))
         wait_time = max(page_count * 30, 300)
+        print(f'{email}: PDF detected with {page_count} pages, waiting for {wait_time} seconds.')
 
         time.sleep(wait_time)
-        print(f'PDF detected with {page_count} pages, waiting for {wait_time} seconds.')
     elif video_elements:
         video = video_elements[0]
         video_duration = driver.execute_script('return arguments[0].duration;', video)
         wait_time = max(video_duration + random.randint(10, 30), 300)
+        print(f'{email}: Video detected with duration {video_duration} seconds, waiting for {wait_time} seconds.')
 
         time.sleep(wait_time)
-        print(f'Video detected with duration {video_duration} seconds, waiting for {wait_time} seconds.')
     else:
         wait_time = random.randint(300, 360)
+        print(f'{email}: No PDF or video detected, waiting for {wait_time} seconds.')
 
         time.sleep(wait_time)
-        print(f'No PDF or video detected, waiting for {wait_time} seconds.')
 
     click_next_page(driver)
     driver.switch_to.default_content()
@@ -75,15 +71,15 @@ def run_for_account(email, password):
 
         input_elements[0].send_keys(email)
         input_elements[2].send_keys(password)
-        clickAuth(driver)
+        driver.find_elements(By.TAG_NAME, 'button')[1].click()
         time.sleep(10)
-        clickAuth(driver)
+        driver.find_elements(By.TAG_NAME, 'button')[1].click()
     elif "authorize" in driver.current_url:
-        clickAuth(driver)
+        driver.find_elements(By.TAG_NAME, 'button')[1].click()
 
-    time.sleep(50)
+    time.sleep(30)
 
-    while True: process_material(driver)
+    while True: process_material(driver, email)
 
 
 if __name__ == '__main__':
